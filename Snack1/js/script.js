c8cee1f6cf24847ae0e237728745d74be558c1d0
javoxpl <liacovone7@outlook.it>
2022-05-25 18:01:00 +0200

    Snack 1 completed (console)

--- Snack1/js/script.js
+++ Snack1/js/script.js
@@ -1,1 +1,27 @@
 /*Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.*/
+
+
+let arrayVuoto = [];
+
+
+let i= 0;
+let somma = 0; 
+
+do {
+let number = parseInt(prompt("inserisci un numero"));
+    arrayVuoto.push(number);
+    
+    
+    somma = arrayVuoto[i] + somma;
+    i++;
+
+
+
+
+}while (somma<49)
+
+console.log(somma);
+console.log(arrayVuoto);
+
+
+
